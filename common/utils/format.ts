f6bc3167be30ab9b9c23fedd1d63a0afa12c2108
Zach R <zarutman+git@gmail.com>
2023-06-27 13:07:39 -0700

    fix: show times correctly

--- common/utils/format.ts
+++ common/utils/format.ts
@@ -1,6 +1,6 @@
 export function formatDuration(duration:number) {
     
     const minutes = Math.floor(duration / 60).toFixed(0);
-    const seconds = (duration % 60).toFixed(1).padStart(2, '0');
+    const seconds = (duration % 60).toFixed(1).padStart(4, '0');
     return `${minutes}:${seconds}`;
 }
