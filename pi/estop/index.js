0e3b0051b965b91c828edd14fafa0ae5c9823a79
Zach R <zarutman+git@gmail.com>
2023-07-06 22:53:27 -0700

    refactor: make pi configuration flexible

--- pi/estop/index.js
+++ pi/estop/index.js
@@ -5,7 +5,7 @@
 
 const { Gpio } = require("onoff");
 const { io } = require("socket.io-client");
-
+const consts = require("./consts.json")
 
 const debounceDelay = 50
 
@@ -45,11 +45,13 @@ class Estop {
     }
 }
 
-const estops = {
-    "R1": new Estop(21, "R1"),
-    "R2": new Estop(13, "R2"),
-    // "R3": new Estop(0, "R3")
-}
+const estops = {}
+
+consts.pins.forEach(([ds, pin]) => {
+    estops[ds] = new Estop(pin, ds)
+})
+
+console.log(estops)
 
 
 const handlePressed = (station) => {
@@ -61,9 +63,9 @@ const handleReleased = (station) => {
     console.log(station, "released")
 }
 
-const socket = io("http://192.168.86.69:3000", {
+const socket = io(consts.socket.host, {
     auth: {
-        key: "fowlfield",
+        key: consts.socket.key,
         role: "estop"
     },
     autoConnect:false,
@@ -75,16 +77,10 @@ socket.on("connect_error", (err) => console.log("connect failed", err.message))
 socket.on("disconnect", (reason) => console.log("disconnect", reason))
 socket.on("queryEstop", (cb) => {
     console.log("querying")
-    cb({
-        "R1":estops["R1"].currentButtonState,
-        "R2": estops["R2"].currentButtonState,
-        "R3": false,
-        "B1": false,
-        "B2": false,
-        "B3": false,
-
-        // "R2":estops["R2"].currentButtonState,
-        // "R3":estops["R3"].currentButtonState
+    const output = {}
+    consts.pins.forEach(([ds, pin]) => {
+        output[ds] = estops[ds].currentButtonState
     })
+    cb(output)
 })
 socket.connect()
