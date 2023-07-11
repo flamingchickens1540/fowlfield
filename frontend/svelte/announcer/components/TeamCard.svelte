a67007bcfc71bc3866cc5591f448e52c601be474
Zach R <zarutman+git@gmail.com>
2023-07-11 16:48:41 -0700

    feat: update formatting on announcer display

--- frontend/svelte/announcer/components/TeamCard.svelte
+++ frontend/svelte/announcer/components/TeamCard.svelte
@@ -2,9 +2,9 @@
     import matchData, { teamList, teamRankings } from "@store"
     export let team_num: number;
     export let alliance: 'red' | 'blue'
-
+    
     const { type } = matchData
-
+    
     let selectionMap:{[key:number]:string} = {
         0: "",
         1: "Captain",
@@ -13,47 +13,75 @@
         4: "Third Pick"   
     }
 </script>
+
+<script context=module lang=ts>
+    function getOrdinal(n) {
+        let ord = 'th';
+        
+        if (n % 10 == 1 && n % 100 != 11) {ord = 'st';}
+        else if (n % 10 == 2 && n % 100 != 12){ord = 'nd';}
+        else if (n % 10 == 3 && n % 100 != 13){ord = 'rd';}
+        
+        return n+ord;
+    }
+</script>
+
 {#if team_num != 0}
-    <div class="{alliance} team-card">
-        <strong>
-            <div class="team-num">{$teamList[team_num]?.displaynum?.get()}</div>
-            <div class="team-info">
-                <div class="team-info-item">Team Name: {$teamList[team_num]?.name?.get()}</div>
-                <div class="team-info-item">Robot Name: {$teamList[team_num]?.robotname?.get()}</div>
-                {#if $type == "qualification"}
-                    <div>Team Rank: {$teamRankings[team_num] + 1}</div>
-                {:else}
-                    <div>Alliance Pick: {selectionMap[$teamList[team_num]?.alliancePosition?.get()]}</div>
-                {/if}
-            </div>
-        </strong>
+<div class="team-card" style="--colorA:{alliance == "red" ? "rgb(218, 56, 50)" :"rgb(42, 100, 173)"}; --colorB:{alliance == "red" ? "hsl(2, 63%, 45%)" :"hsl(213, 64%, 35%)"}">
+    <div class="team-num">{$teamList[team_num]?.displaynum?.get()}</div>
+    <div class=conts>
+        <div class="team-info-label">Team Name</div><div class=team-info-item>{$teamList[team_num]?.name?.get()}</div>
+        <div class="team-info-label">Robot Name</div><div class=team-info-item>{$teamList[team_num]?.robotname?.get()}</div>
+        {#if $type == "qualification"}
+        <div class=team-info-label>Team Rank</div><div class=team-info-item>{getOrdinal($teamRankings[team_num] + 1)}</div>
+        {:else}
+        <div class=team-info-label>Alliance Pick</div><div class=team-info-item>{selectionMap[$teamList[team_num]?.alliancePosition?.get()]}</div>
+        {/if}
     </div>
+</div>
 {/if}
 
-<style>
-    .team-info-item {
-        padding-bottom: 1%;
+<style lang=scss>
+    .conts {
+        display:contents;
     }
-    .team-info {
+    .team-info-item {
         font-size: x-large;
     }
+    .team-info-label {
+        font-size: x-large;
+    }
+    
+    .team-info-item, .team-info-label {
+        height:100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        &:nth-child(4n+3), &:nth-child(4n+4) {
+            background-color:var(--colorB);
+        }
+    }
     .team-num {
-        font-size: xxx-large;
-        padding-bottom: 2%;
+        background-color:var(--colorB);
+        font-size: 70px;
+        height:100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        grid-row:1;
+        grid-column:1/ span 2;
+        border-top-left-radius: 25px;
+        border-top-right-radius: 25px;
     }
-
+    
     .team-card {
-        padding-bottom: 3%;
-        padding-top: 3%;
-    }
-
-    .red {
-        background-color: rgb(218, 56, 50);
+        display:grid;
+        grid-template-columns: auto auto;
+        grid-auto-flow: rows;
+        grid-template-rows: 100px auto auto auto;
         border-radius: 25px;
-    }
-
-    .blue {
-        background-color: rgb(42, 100, 173);
-        border-radius: 25px;
+        background-color:var(--colorA);
+        margin-bottom:10px;
+        // height:100%;
     }
 </style>
