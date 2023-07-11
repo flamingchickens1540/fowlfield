127bac56d0d99f1cb618f6dcb3596df8371989e4
Zach R <zarutman+git@gmail.com>
2023-07-11 12:00:10 -0700

    feat: show more expressive lights while disconnected

--- pi/light/index.js
+++ pi/light/index.js
@@ -64,6 +64,28 @@ const timer = setInterval(() => {
 },500)
 
 
+let disconnectedBlinkIndex = 0
+let disconnectedBlinkDown = true
+let disconnectedBlinker;
+function launchDisconnectedBlinker() {
+    clearInterval(disconnectedBlinker)
+    disconnectedBlinker =setInterval(() => {
+        lightList.forEach((light, index) => {
+            light.setSolid(index == disconnectedBlinkIndex ? 1 : 0)
+        })
+        if (disconnectedBlinkIndex == lightList.length-1) {
+            disconnectedBlinkDown = false
+        } else if (disconnectedBlinkIndex == 0) {
+            disconnectedBlinkDown = true
+        }
+        if (disconnectedBlinkDown) {
+            disconnectedBlinkIndex++
+        } else {
+            disconnectedBlinkIndex--
+        }
+    },200)
+}
+
 
 
 const socket = io(consts.socket.host, {
@@ -75,15 +97,20 @@ const socket = io(consts.socket.host, {
 });
 // socket.onAny((...data) => console.log(...data))
 console.log("starting")
+launchDisconnectedBlinker()
+
 socket.on("connect", () => {
     console.log("connected")
+    clearInterval(disconnectedBlinker)
     consts.pins.forEach(([color, pin]) => {
         lights[color].setSolid(0)
     })
+
 })
 socket.on("connect_error", (err) => console.log("connect failed", err.message))
-socket.on("disconnect", (reason) => console.log("disconnect", reason))
+socket.on("disconnect", (reason) => {launchDisconnectedBlinker(); console.log("disconnect", reason)})
 socket.on("setLight", (color, state) => {
+    clearInterval(disconnectedBlinker)
     const light = lights[color]
     let result
     switch (state) {
