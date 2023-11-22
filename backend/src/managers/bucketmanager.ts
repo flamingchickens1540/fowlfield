444355a006fa11c841e673d0e66ddf3163844c78
Zach R <zarutman+git@gmail.com>
2023-11-22 12:47:16 -0800

    feat: send bucket instructions (#3)

---
+++ backend/src/managers/bucketmanager.ts
@@ -0,0 +1,41 @@
+import { Socket } from 'socket.io';
+import { DriverStation } from '../../../common/types/types';
+import { BucketPattern, ClientToServerEvents, ServerToClientEvents } from '@fowltypes';
+import rootLogger from 'logger';
+import { stat } from 'fs';
+
+const logger = rootLogger.getLogger("bucket")
+const buckets:{[key in DriverStation]:Socket<ClientToServerEvents, ServerToClientEvents>|null} = {
+    R1: null,
+    R2: null,
+    R3: null,
+    B1: null,
+    B2: null,
+    B3: null
+}
+
+export function registerBucket(station:DriverStation, socket:Socket<ClientToServerEvents, ServerToClientEvents>) {
+    logger.log("registering bucket", station)
+    if  (buckets[station] != null) {
+        logger.warn("bucket already registered for station", station)
+        setPattern(station, BucketPattern.RED_ALLIANCE) // TODO: make a more explicit error pattern
+        buckets[station].disconnect(true)
+    }
+    buckets[station] = socket
+    setPattern(station, BucketPattern.RAINBOW)
+}
+
+export function setPattern(station:DriverStation, pattern:BucketPattern) {
+    const bucket = buckets[station]
+    if (bucket == null) {
+        logger.warn("no bucket for station", station)
+        return
+    }
+    bucket.emit("setBucketState", pattern)
+}
+
+export function setPatternAll(pattern:BucketPattern) {
+    for (const station in buckets) {
+        setPattern(station as DriverStation, pattern)
+    }
+}
