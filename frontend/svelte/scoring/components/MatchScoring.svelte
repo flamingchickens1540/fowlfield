3fca38691b22d1355d643d5a70330eab0ac016d3
Zach R <zarutman+git@gmail.com>
2023-11-28 22:24:50 -0800

    fix: actually allow refs to set taxi and park states💀

--- frontend/svelte/scoring/components/MatchScoring.svelte
+++ frontend/svelte/scoring/components/MatchScoring.svelte
@@ -1,140 +1,185 @@
 <script lang="ts">
-    import type { DriverStation, RobotHitState } from "@fowltypes";
-    import { driverstations } from "../Scoring.svelte";
-    import matchData, { dsStatuses, teamList } from "@store";
-    import { derived, get } from "svelte/store";
-    import socket from "@socket";
-    import { onMount } from "svelte/internal";
-    import EstopConfirm from "./EstopConfirm.svelte";
-    
-    export let station:1|2|3
-    export let alliance:'red'|'blue'
-    export const pos = `${alliance}${station}`
-    export const dsposstring = (alliance == "red" ? "R":"B")+station as DriverStation
-    const robot = derived([matchData[pos], teamList], ([$teamid, $teams]) => $teams[$teamid]?.displaynum?.get() ?? "0")
-    const scoreBreakdown         = alliance== "red" ? matchData.redScoreBreakdown : matchData.blueScoreBreakdown
-    const scoreBreakdownOpponent = alliance== "red" ? matchData.blueScoreBreakdown : matchData.redScoreBreakdown
-    
-    $: if ($scoreBreakdown == null) {$scoreBreakdown = {autoBunnyCount:0, autoTaxiBonus:[false,false,false], finalBunnyCount:0, targetHits:[0,0,0], endgameParkBonus:[false,false,false], fouls:[]}}
-    $: if ($scoreBreakdownOpponent == null) {$scoreBreakdownOpponent = {autoBunnyCount:0, autoTaxiBonus:[false,false,false], finalBunnyCount:0, targetHits:[0,0,0], endgameParkBonus:[false,false,false], fouls:[]}}
-    let lastHitTime:number = 0
-    let undoStopwatchValue = "-"
-    function registerHit() {
-        lastHitTime = Date.now()
-        socket.emit("registerHit", dsposstring)
-    }
-    
-    function undoHit() {
-        lastHitTime = 0
-        socket.emit("undoHit", dsposstring)
-    }
-    function registerFoul(value:number) {
-        scoreBreakdownOpponent.update((breakdown) => {breakdown.fouls.push({"robot": get(matchData[pos]), timestamp: Date.now(), value}); return breakdown})
-    }
-    async function estop() {
-        const didConfirm = await estopconfirm.confirm()
-        if (didConfirm) {
-            socket.emit("estop", driverstations[pos])
-        }
-    }
-    let estopconfirm: EstopConfirm
-    setInterval(() => {
-        const time = Math.round((Date.now() - lastHitTime)/1000)
-        undoStopwatchValue = time > 15 ? "—" : time+"s"
-    }, 500)
+	import type { DriverStation, RobotHitState } from "@fowltypes";
+	import { driverstations } from "../Scoring.svelte";
+	import matchData, { dsStatuses, teamList } from "@store";
+	import { derived, get } from "svelte/store";
+	import socket from "@socket";
+	import { onMount } from "svelte/internal";
+	import EstopConfirm from "./EstopConfirm.svelte";
+	import writableDerived from "svelte-writable-derived";
 
+	export let station: 1 | 2 | 3;
+	export let alliance: "red" | "blue";
+	export const pos = `${alliance}${station}`;
+	export const dsposstring = ((alliance == "red" ? "R" : "B") + station) as DriverStation;
+	const robot = derived([matchData[pos], teamList], ([$teamid, $teams]) => $teams[$teamid]?.displaynum?.get() ?? "0");
+	const scoreBreakdown = alliance == "red" ? matchData.redScoreBreakdown : matchData.blueScoreBreakdown;
+	const scoreBreakdownOpponent = alliance == "red" ? matchData.blueScoreBreakdown : matchData.redScoreBreakdown;
 
-    
-    let disableHitButton = false
+	$: if ($scoreBreakdown == null) {
+		$scoreBreakdown = { autoBunnyCount: 0, autoTaxiBonus: [false, false, false], finalBunnyCount: 0, targetHits: [0, 0, 0], endgameParkBonus: [false, false, false], fouls: [] };
+	}
+	$: if ($scoreBreakdownOpponent == null) {
+		$scoreBreakdownOpponent = { autoBunnyCount: 0, autoTaxiBonus: [false, false, false], finalBunnyCount: 0, targetHits: [0, 0, 0], endgameParkBonus: [false, false, false], fouls: [] };
+	}
+	let lastHitTime: number = 0;
+	let undoStopwatchValue = "-";
+	function registerHit() {
+		lastHitTime = Date.now();
+		socket.emit("registerHit", dsposstring);
+	}
 
-    dsStatuses.subscribe((statuses) => {
-        if (statuses == null) {return}
-        const station  = statuses[dsposstring]
-        disableHitButton = station.bypassed ? false : !station.robotConnected || station.isEstopped || !station.enabled;
-    })
+	function undoHit() {
+		lastHitTime = 0;
+		socket.emit("undoHit", dsposstring);
+	}
+	function registerFoul(value: number) {
+		scoreBreakdownOpponent.update((breakdown) => {
+			breakdown.fouls.push({ robot: get(matchData[pos]), timestamp: Date.now(), value });
+			return breakdown;
+		});
+	}
+	async function estop() {
+		const didConfirm = await estopconfirm.confirm();
+		if (didConfirm) {
+			socket.emit("estop", driverstations[pos]);
+		}
+	}
+	let estopconfirm: EstopConfirm;
+	setInterval(() => {
+		const time = Math.round((Date.now() - lastHitTime) / 1000);
+		undoStopwatchValue = time > 15 ? "—" : time + "s";
+	}, 500);
+
+	let disableHitButton = false;
+
+	dsStatuses.subscribe((statuses) => {
+		if (statuses == null) {
+			return;
+		}
+		const station = statuses[dsposstring];
+		disableHitButton = station.bypassed ? false : !station.robotConnected || station.isEstopped || !station.enabled;
+	});
+
+	const didTaxi = writableDerived(
+		scoreBreakdown,
+		($breakdown) => $breakdown.autoTaxiBonus[station - 1],
+		(newVal, $breakdown) => {
+			$breakdown.autoTaxiBonus[station - 1] = newVal;
+			return $breakdown;
+		}
+	);
+	const didPark = writableDerived(
+		scoreBreakdown,
+		($breakdown) => $breakdown.endgameParkBonus[station - 1],
+		(newVal, $breakdown) => {
+			$breakdown.endgameParkBonus[station - 1] = newVal;
+			return $breakdown;
+		}
+	);
 </script>
 
-<div class="scoring-container" style="--buttoncolor:{alliance == "red" ? 'rgb(122, 0, 0)' : 'rgb(0, 45, 122)'}">
-    <EstopConfirm bind:this={estopconfirm}>
-        <span slot=robot>{$robot}</span>
-    </EstopConfirm>
-    <button style="grid-column:1/span 2; height:100%; background-color:var(--buttoncolor); font-size:25px;display:flex;align-items:center;justify-content:center;" on:click={() => window.location.pathname = "/scoring/"+prompt("What station are you")}>
-        <span>{pos.toUpperCase()} - {$robot}</span>
-    </button>
-    <div style="font-size:40px;">
-        <div style="width:100%;height:calc(50% - 10px);padding-bottom:10px;margin:0;">
-            <button class="btn" style="height:100%; width:100%;margin:0;" on:click={() => registerFoul(5)}>5pt Foul</button>
-        </div>
-        <div style="width:100%;height:calc(50% - 10px);padding-top:10px;margin:0;">
-            <button class="btn" style="height:100%; width:100%;margin:0;" on:click={() => registerFoul(10)}>10pt Foul</button>
-        </div>
-    </div>
-    <div style=display:flex;justify-content:space-evenly;flex-direction:column>
-        <span style="font-size:30px;">Bunnies</span>
-        <div>
-        <span style="font-size:20px;">Auto</span>
-        <div class=bunnybuttons style="width:100%">
-            <button class="red-button bunny-btn" on:click={() => $scoreBreakdown.autoBunnyCount--}>-</button>
-            <input type="number" bind:value={$scoreBreakdown.autoBunnyCount} />
-            <button class="green-button bunny-btn" on:click={() => $scoreBreakdown.autoBunnyCount++}>+</button>
-        </div>
-    </div>
-    <div>
-        <span style="font-size:20px;">Endgame</span>
-        <div class=bunnybuttons style="width:100%">
-            <button class="red-button bunny-btn" on:click={() => $scoreBreakdown.finalBunnyCount--}>-</button>
-            <input type="number" bind:value={$scoreBreakdown.finalBunnyCount} />
-            <button class="green-button bunny-btn" on:click={() => $scoreBreakdown.finalBunnyCount++}>+</button>
-        </div>
-    </div>
-</div>
-    <div>
-        <button class="btn" style="width:100%;height:100%;;box-sizing:border-box;margin:0;font-size:60px;background-color:black;border:1px solid #a0a0a0" on:click={estop}>Estop</button>
-    </div>
-    <div class="hit-tracker">
-        <button class="red-button btn" style="width:100%;height:70%;;box-sizing:border-box;margin:0;font-size:60px;" on:click={registerHit} disabled={disableHitButton}>Hit ({$scoreBreakdownOpponent.targetHits[station-1] ?? 0})</button>
-        <div style="width:100%;height:calc(30% - 10px);padding-top:10px;margin:0;">
-            <button class="btn" style="width:100%;height:100%;margin:0;font-size:40px;background-color:orange" on:click={undoHit}>Undo ({undoStopwatchValue})</button>
-        </div>
-    </div>
+<div class="scoring-container" style="--buttoncolor:{alliance == 'red' ? 'rgb(122, 0, 0)' : 'rgb(0, 45, 122)'}">
+	<EstopConfirm bind:this={estopconfirm}>
+		<span slot="robot">{$robot}</span>
+	</EstopConfirm>
+	<button style="grid-column:1/span 2; height:100%; background-color:var(--buttoncolor); font-size:25px;display:flex;align-items:center;justify-content:center;" on:click={() => (window.location.pathname = "/scoring/" + prompt("What station are you"))}>
+		<span>{pos.toUpperCase()} - {$robot}</span>
+	</button>
+	<div style="font-size:40px;">
+		<div style="width:100%;height:calc(50% - 10px);padding-bottom:10px;margin:0;">
+			<button class="btn" style="height:100%; width:100%;margin:0;" on:click={() => registerFoul(5)}>5pt Foul</button>
+		</div>
+		<div style="width:100%;height:calc(50% - 10px);padding-top:10px;margin:0;">
+			<button class="btn" style="height:100%; width:100%;margin:0;" on:click={() => registerFoul(10)}>10pt Foul</button>
+		</div>
+	</div>
+	<div style="display:flex;justify-content:space-between;flex-direction:row;padding:20px;;">
+		<div style="display:flex;justify-content:space-evenly;flex-direction:column">
+			<div style="background-color:#433e44;padding:10px;border-radius:5px;">
+				<span style="font-size:25px;">Auto Bunnies</span>
+				<div class="bunnybuttons" style="width:100%">
+					<button class="red-button bunny-btn" on:click={() => $scoreBreakdown.autoBunnyCount--}>-</button>
+					<input type="number" bind:value={$scoreBreakdown.autoBunnyCount} />
+					<button class="green-button bunny-btn" on:click={() => $scoreBreakdown.autoBunnyCount++}>+</button>
+				</div>
+			</div>
+			<div style="background-color:#433e44;padding:10px;border-radius:5px;">
+				<span style="font-size:25px;">Endgame Bunnies</span>
+				<div class="bunnybuttons" style="width:100%">
+					<button class="red-button bunny-btn" on:click={() => $scoreBreakdown.finalBunnyCount--}>-</button>
+					<input type="number" bind:value={$scoreBreakdown.finalBunnyCount} />
+					<button class="green-button bunny-btn" on:click={() => $scoreBreakdown.finalBunnyCount++}>+</button>
+				</div>
+			</div>
+		</div>
+		<div id="mobility" style="display:flex;justify-content:space-evenly;flex-direction:column">
+			<button on:click={() => ($didTaxi = !$didTaxi)} style="background-color:{$didTaxi ? '#727e72' : '#1a1a1a'}">Taxi</button>
+			<button on:click={() => ($didPark = !$didPark)} style="background-color:{$didPark ? '#727e72' : '#1a1a1a'}">Park</button>
+		</div>
+	</div>
+
+	<div>
+		<button class="btn" style="width:100%;height:100%;;box-sizing:border-box;margin:0;font-size:60px;background-color:black;border:1px solid #a0a0a0" on:click={estop}>Estop</button>
+	</div>
+	<div class="hit-tracker">
+		<button class="red-button btn" style="width:100%;height:70%;;box-sizing:border-box;margin:0;font-size:60px;" on:click={registerHit} disabled={disableHitButton}>Hit ({$scoreBreakdownOpponent.targetHits[station - 1] ?? 0})</button>
+		<div style="width:100%;height:calc(30% - 10px);padding-top:10px;margin:0;">
+			<button class="btn" style="width:100%;height:100%;margin:0;font-size:40px;background-color:orange" on:click={undoHit}>Undo ({undoStopwatchValue})</button>
+		</div>
+	</div>
 </div>
 
 <style lang="scss">
-    .scoring-container {
-        display: grid;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        padding: 5px;
-        max-height: 100vh;
-        gap:40px;
-        overflow: hidden;
-        grid-template-rows: 50px auto auto;
-        grid-template-columns: auto auto;
-        grid-auto-flow:row;
-        > * {
-            border: 1px white dashed;
-        }
-    }
-    .btn {
-        background-color:var(--buttoncolor,rgb(122,0,0));
-    }
-    .red-button {
-        background-color: rgb(122, 0, 0);
-    }
-    .green-button {
-        background-color: green;
-    }
-    .bunnybuttons > *{
-        height:60px;
-        font-size: 20px;
-    }
+	.scoring-container {
+		display: grid;
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		padding: 5px;
+		max-height: 100vh;
+		gap: 40px;
+		overflow: hidden;
+		grid-template-rows: 50px auto auto;
+		grid-template-columns: 35% 60%;
+		grid-auto-flow: row;
+		> * {
+			border: 1px white dashed;
+		}
+	}
+	.btn {
+		background-color: var(--buttoncolor, rgb(122, 0, 0));
+	}
+	.red-button {
+		background-color: rgb(122, 0, 0);
+	}
+	.green-button {
+		background-color: green;
+	}
+	.bunnybuttons > * {
+		height: 120px;
+		font-size: 40px;
+	}
 
-    .bunny-btn {
-        width:80px;
-        text-align:center;
-    }
+	.bunnybuttons input {
+		width: 200px;
+		text-align: center;
+	}
 
+	.bunny-btn {
+		width: 130px;
+
+		text-align: center;
+	}
+
+	#mobility {
+		width: 400px;
+		button {
+			font-size: 50px;
+			width: 100%;
+			height: 40%;
+		}
+	}
 </style>
